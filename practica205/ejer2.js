796386f339b7ba7f1aab9082effd2b3820d1dbea
adrireca <reyes3190@gmail.com>
2022-09-28 20:40:43 +0200

    practica205 (4)

--- practica205/ejer2.js
+++ practica205/ejer2.js
@@ -3,23 +3,48 @@
 // window.onload = temporizador;
 
 let minutos = 1;
-let segundos = 5;
+let segundos = 3;
 
 
-function temporizador(seg) {
+function temporizador(min,seg) {
     let result = "";
-    setInterval(() => {
-        if (seg < 0 || seg > 59) {
-            result = "ERROR. Parámetros no válidos.";
+
+
+    let id = setInterval(() => {
+
+        /*
+        for(let i=0; i<arguments.length; i++){
+
+            
+            if(isNaN(arguments[i]) || arguments[i] < 0 || arguments[i] > 59){
+               console.log("ERROR. Introduce parámetros válidos");
+            }else if(arguments[i] == 0){
+                console.log("Final");
+            }else if(arguments[1] == 0){
+                arguments[0]--;
+                arguments[1] = 59;
+            }
+            
+        }*/       
+        if(isNaN(min) || isNaN(seg) || seg < 0 || min < 0 || seg > 59 || min > 59){
+            console.log("ERROR. Introduce parámetros válidos");
+        }else if (seg == 0) {
+            min--;
+            seg = 59;
+        }else if(min == 0 && seg == 0){
+            min = 0;
+            seg = 0;
+            clearInterval(id);
         }
         else {
-            console.log(seg);
+            console.log(`${min}:${seg}`);
             seg -= 1;
             result = seg;
         }
+        
     }, 1000);
 
     return result;
 }
 
-temporizador(segundos);
+temporizador(minutos,segundos);
