d14d33bebc19ec2e5eef0efdc0520f5afa840639
adrireca <reyes3190@gmail.com>
2022-10-21 20:45:41 +0200

    pract. 303 (3)

--- practica303/js/gestor.js
+++ practica303/js/gestor.js
@@ -3,56 +3,98 @@
 let d = window.document;
 let botonAnyadir = d.querySelector(".add"); //botón añadir tarea.
 let botonAcabar = d.querySelectorAll("#pendientes .end"); //botón acabar tarea.
-let botonBorrar = d.querySelectorAll('#pendientes .del');
+// let botonBorrar = d.querySelectorAll('#pendientes .del');
 let textArea = d.querySelector("#tareas textarea"); //textarea.
 let divPendientes = d.getElementById("pendientes"); //div tareas pendientes.
 let divAcabadas = d.getElementById("acabadas"); //div tareas acabadas.
 
 //función plantilla de la tarea a añadir.
 function templateTareaAnyadir(texto) {
-  let tarea = `<div class="tarea">
-                            <p>${texto}</p>
-                            <p class="botones">
-                                <input type="button" value="Borrar" class="del" />
-                                <input type="button" value="Acabar" class="end" />
-                            </p>
-                        </div>`;
-  return tarea;
+  let divTarea = d.createElement('div');
+  divTarea.classList.add('tarea');
+  let pTexto = d.createElement('p');
+  pTexto.innerText = texto;
+  divTarea.appendChild(pTexto);
+  let pBotones = d.createElement('p');
+  pBotones.classList.add('botones');
+  divTarea.appendChild(pBotones);
+  let btnBorrar = d.createElement('input');
+  btnBorrar.classList.add('del');
+  btnBorrar.type = "button";
+  btnBorrar.value = 'Borrar';
+  pBotones.appendChild(btnBorrar);
+  let btnAcabar = d.createElement('input');
+  btnAcabar.classList.add('end');
+  btnAcabar.type = "button";
+  btnAcabar.value = 'Acabar';
+  pBotones.append(btnAcabar);
+
+  btnBorrar.onclick = borrarTarea;
+  btnAcabar.onclick = acabarTarea;
+  // btnBorrar.addEventListener("click",borrarTarea,false);
+  // btnAcabar.addEventListener("click",acabarTarea,false);
+
+  
+  return divTarea;
 }
 
 //función plantilla de la tarea acabada.
-function templateTareaAcabada(texto) {
-  let tarea = `<div class="acabada">
-                            <p>${texto}</p>
-                            <p class="botones">
-                              <input type="button" value="Archivar" class="del" />
-                              <input type="button" value="Volver" class="end" />
-                            </p>
-                          </div>`;
-  return tarea;
-}
+// function templateTareaAcabada(texto) {
+//   let tarea = `<div class="acabada">
+//                             <p>${texto}</p>
+//                             <p class="botones">
+//                               <input type="button" value="Archivar" class="del" />
+//                               <input type="button" value="Volver" class="end" />
+//                             </p>
+//                           </div>`;
+//   return tarea;
+// }
 
 //función añadir tarea.
-botonAnyadir.addEventListener("click", () => {
+botonAnyadir.onclick = anyadirTarea;
+
+function anyadirTarea(){
   if (textArea.value !== "") {
     //añade la tarea solo si ésta tiene contenido.
-    divPendientes.innerHTML += templateTareaAnyadir(textArea.value);
+    divPendientes.appendChild(templateTareaAnyadir(textArea.value));
     textArea.value = ""; //limpio el textarea.
     textArea.focus(); //foco en el textarea.
   }
-});
+}
 
 //función acabar tarea.
-botonAcabar.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    divAcabadas.innerHTML += templateTareaAcabada(textArea.value);
-  });
-});
+function acabarTarea(tarea){
+  let divTarea = tarea.target.parentElement.parentElement;
+  divTarea.style.visibilty = 'hidden';
+  divTarea.classList.add('acabada');
+  divTarea.lastChild.firstChild.value = "Archivar";
+  divTarea.lastChild.firstChild.nextSibling.value = "Volver";
+  divAcabadas.appendChild(divTarea);
+  let btnVolver = divTarea.lastChild.firstChild.nextSibling;
+  btnVolver.onclick = volverTarea;
+  // btnVolver.addEventListener('click',volverTarea,false);
+}
+
+//función tarea acabada de vuelta a pendientes.
+function volverTarea(tarea){
+  let divTarea = tarea.target.parentElement.parentElement;
+  divTarea.classList.remove('acabada');
+  divTarea.classList.add('tarea');
+  divTarea.lastChild.firstChild.value = "Borrar";
+  divTarea.lastChild.firstChild.nextSibling.value = "Acabar";
+  divPendientes.appendChild(divTarea);
+  // let btnArchivar = divTarea.lastChild.firstChild;
+  // console.log(btnArchivar);
+  // btnArchivar.addEventListener('click',archivarTarea,false);
+}
+
+//función archivar tarea.
+function archivarTarea(tarea){
+  console.log(tarea.target);
+}
 
 
 //función eliminar tarea.
-botonBorrar.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    btn.parentElement.parentElement.remove();
-  });
-});
+function borrarTarea(tarea){
+  tarea.target.parentElement.parentElement.remove();
+}
