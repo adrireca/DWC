12aa60683e5669e1160899c7043f7323bff7ab7a
Adrian <reyes3190@gmail.com>
2022-10-21 12:00:45 +0200

    pract303 (2)

--- practica303/js/gestor.js
+++ practica303/js/gestor.js
@@ -2,13 +2,14 @@
 
 let d = window.document;
 let botonAnyadir = d.querySelector(".add"); //botón añadir tarea.
-let botonAcabar = d.querySelector(".end"); //botón acabar tarea.
+let botonAcabar = d.querySelectorAll("#pendientes .end"); //botón acabar tarea.
+let botonBorrar = d.querySelectorAll('#pendientes .del');
 let textArea = d.querySelector("#tareas textarea"); //textarea.
 let divPendientes = d.getElementById("pendientes"); //div tareas pendientes.
-let divAcabadas = d.getElementById('acabadas'); //div tareas acabadas.
+let divAcabadas = d.getElementById("acabadas"); //div tareas acabadas.
 
-//función plantilla de la tarea.
-function templateTarea(texto) {
+//función plantilla de la tarea a añadir.
+function templateTareaAnyadir(texto) {
   let tarea = `<div class="tarea">
                             <p>${texto}</p>
                             <p class="botones">
@@ -16,17 +17,42 @@ function templateTarea(texto) {
                                 <input type="button" value="Acabar" class="end" />
                             </p>
                         </div>`;
-    return tarea;
+  return tarea;
+}
+
+//función plantilla de la tarea acabada.
+function templateTareaAcabada(texto) {
+  let tarea = `<div class="acabada">
+                            <p>${texto}</p>
+                            <p class="botones">
+                              <input type="button" value="Archivar" class="del" />
+                              <input type="button" value="Volver" class="end" />
+                            </p>
+                          </div>`;
+  return tarea;
 }
 
 //función añadir tarea.
 botonAnyadir.addEventListener("click", () => {
-  divPendientes.innerHTML += templateTarea(textArea.value);
-  textArea.value = "";
-  textArea.focus();
+  if (textArea.value !== "") {
+    //añade la tarea solo si ésta tiene contenido.
+    divPendientes.innerHTML += templateTareaAnyadir(textArea.value);
+    textArea.value = ""; //limpio el textarea.
+    textArea.focus(); //foco en el textarea.
+  }
 });
 
 //función acabar tarea.
-botonAcabar.addEventListener("click", () => {
-  divAcabadas.innerHTML += templateTarea(textArea.value);
+botonAcabar.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    divAcabadas.innerHTML += templateTareaAcabada(textArea.value);
+  });
+});
+
+
+//función eliminar tarea.
+botonBorrar.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    btn.parentElement.parentElement.remove();
+  });
 });
