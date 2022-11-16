6cc197af480ee4c86901f74ca1f5e8e35eda49f1
Adrian <reyes3190@gmail.com>
2022-11-16 14:27:13 +0100

    pract408_3

--- practica408/src/lista_compra/Lista_compra.js
+++ practica408/src/lista_compra/Lista_compra.js
@@ -9,51 +9,84 @@ export const Lista_compra = () => {
     unidades: "",
   };
 
-  let contador = 0;
-  let listaIndividual = [];
+  const [listaState, setLista] = useState(listaCompraInicial);
+  let [rellenarCamposState, setRellenarCampos] = useState(
+    "No hay productos en la lista"
+  );
 
-  const [lista, setLista] = useState(listaCompraInicial);
-
-  const almacenarProducto = () => {
-    listaIndividual[contador] = lista;
-    contador++;
-    console.log(listaIndividual);
-  }
-
-  const anyadirProducto = (e) => {
-    const { name, value } = e.target;
-    setLista({...lista, [name]: value});
+  const recogerDatos = (e) => {
+    e.preventDefault();
+    let target = e.target;
+    let nombre = target.nombre.value;
+    let descripcion = target.descripcion.value;
+    let precio = target.precio.value;
+    let unidades = target.unidades.value;
+    let listaState = {
+      id: new Date().getTime(),
+      nombre,
+      descripcion,
+      precio,
+      unidades,
+    };
+    rellenarCamposState = "Rellena todos los campos";
+    setLista(listaState);
+    setRellenarCampos(rellenarCamposState);
   };
+
+  // const cambiarDatos = (e) => {
+  //   const { name, value } = e.target;
+  //   setLista({ ...listaState, [name]: value });
+  // };
 
   return (
     <React.Fragment>
       <div className="container">
         <h1>Lista de la compra</h1>
-       
-        <label htmlFor="id">Id:</label>
-          <input
-            name="id"
-            className="conEstilo"
-            type="text"
-            placeholder="Escribe tu id."
-            value={lista.id}
-            onChange={anyadirProducto}
-          />
+        <form onSubmit={recogerDatos}>
           <label htmlFor="nombre">Nombre:</label>
           <input
             name="nombre"
             className="conEstilo"
             type="text"
             placeholder="Escribe tu nombre."
-            value={lista.nombre}
-            onChange={anyadirProducto}
           />
-          {<button onClick={almacenarProducto}>Añadir</button> }
-        
-
+          <label htmlFor="descripcion">Descripción:</label>
+          <input
+            name="descripcion"
+            className="conEstilo"
+            type="text"
+            placeholder="Escribe tu descripción."
+          />
+          <label htmlFor="precio">Precio:</label>
+          <input
+            name="precio"
+            className="conEstilo"
+            type="text"
+            placeholder="Escribe el precio."
+          />
+          <label htmlFor="unidades">Precio:</label>
+          <input
+            name="unidades"
+            className="conEstilo"
+            type="text"
+            placeholder="Escribe las unidades."
+          />
+          <button type="submit">Añadir</button>
+        </form>
         <div className="resultado">
-            {/* <p>{lista.id}</p>
-            <p>{lista.nombre}</p> */}
+          {listaState.nombre.length >= 1 &&
+          listaState.descripcion.length >= 1 &&
+          listaState.precio.length >= 1 &&
+          listaState.unidades.length >= 1 ? (
+            <ul>
+              <li>{listaState.nombre}</li>
+              <li>{listaState.descripcion}</li>
+              <li>{listaState.precio}</li>
+              <li>{listaState.unidades}</li>
+            </ul>
+          ) : (
+            <p>{rellenarCamposState}</p>
+          )}
         </div>
       </div>
     </React.Fragment>
